8c65a0db4efeaa5158d4882d1c64107d2baa7196
Jon Ericson <jon@jlericson.com>
2019-05-21 21:55:57 -0700

    Grab my version of feedvalidator another way.

--- test/test.rb
+++ test/test.rb
@@ -3,14 +3,19 @@ require 'digest/md5'
 
 class TestDocsDump < Minitest::Test
   def setup
-    if !Dir.exist?('feedvalidator')      
-      system("git clone https://github.com/jericson/feedvalidator.git")
+    if !Dir.exist?('feedvalidator-master')      
+      #system("git clone https://github.com/jericson/feedvalidator.git")
+      if !File.exist?('master.zip')
+        system("wget https://github.com/jericson/feedvalidator/archive/master.zip")
+        assert_equal $?, 0
+      end
+      system("unzip master.zip")
       assert_equal $?, 0
     end
   end
 
   def test_valid_feed
-    system("python feedvalidator/src/demo.py https://unicorn-meta-zoo.github.io/index.rss")
+    system("python feedvalidator-master/src/demo.py https://unicorn-meta-zoo.github.io/index.rss")
     assert_equal $?, 0
   end
 end
