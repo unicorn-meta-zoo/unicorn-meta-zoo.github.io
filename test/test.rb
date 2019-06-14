8dd6c56de61fe7601c96cd057185bb524d2bf221
Jon Ericson <jon@jlericson.com>
2019-06-14 09:26:18 -0700

    Fix the test.

--- test/test.rb
+++ test/test.rb
@@ -4,14 +4,14 @@ require 'digest/md5'
 class TestDocsDump < Minitest::Test
   def setup
     #system("ls -l *")
-    if !File.exist?('feedvalidator-master/src/demo.py')      
+    if !File.exist?('feedvalidator/src/demo.py')      
       system("git clone https://github.com/jericson/feedvalidator.git")
       assert_equal $?, 0
     end
   end
 
   def test_valid_feed
-    system("python feedvalidator-master/src/demo.py https://unicorn-meta-zoo.github.io/index.rss")
+    system("python feedvalidator/src/demo.py https://unicorn-meta-zoo.github.io/index.rss")
     assert_equal $?, 0
   end
 end
