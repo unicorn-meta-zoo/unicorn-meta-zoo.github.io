8c8776a909fba9420cb6c9dd29a48244c2a80f9a
Jon Ericson <jon@jlericson.com>
2019-08-13 10:58:58 -0700

    Workaround for https://github.com/rubys/feedvalidator/pull/21 so that Travis CI tests work.

--- test/test.rb
+++ test/test.rb
@@ -11,7 +11,7 @@ class TestDocsDump < Minitest::Test
   end
 
   def test_valid_feed
-    system("python feedvalidator/src/demo.py https://unicorn-meta-zoo.github.io/index.rss")
+    system("LANGUAGE=en python feedvalidator/src/demo.py https://unicorn-meta-zoo.github.io/index.rss")
     assert_equal $?, 0
   end
 end
